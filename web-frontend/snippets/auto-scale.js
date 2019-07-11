0ff89e146e929ba51d718695ea5e77cb94414d5e
levinit <levinit@outlook.com>
2019-07-11 09:03:03 +0800

    自动缩放字体大小js

--- web-frontend/snippets/auto-scale.js
+++ web-frontend/snippets/auto-scale.js
@@ -1,24 +1,47 @@
-//根据视口大小设置基准字体大小
-document.querySelector('html').style.fontSize = getFontSize();
+//a. 加载页面时根据视口大小设置基准字体大小
+setBaseFontSize()
 
-//更改视口大小时动态调整基准字体大小
-window.onresize = function () {
-   //eslint-disable-next-line
-  var viewportWidth=document.documentElement.scrollWidth || document.body.scrollWidth;
-  document.querySelector('html').style.fontSize = getFontSize(viewportWidth);
-};
+//b. 更改视口大小（浏览器窗口大小）时动态调整基准字体大小
+window.addEventListener('resize', setBaseFontSize, false)
 
-function getFontSize(viewportWidth) {
-  if (!viewportWidth) {
-    viewportWidth =
-      document.documentElement.scrollWidth ||
-      document.documentElement.getBoundingClientRect().width||
-      document.body.scrollWidth||
-      document.body.getBoundingClientRect().width;
+//c. 缩放浏览器时调整
+
+// 提示：浏览器缩放后，页面的documentElement元素的尺寸会随之反比例变化，因此无需获取window.devicePixelRatio缩放比例来调整大小
+// 没有单独对浏览器缩放的监测事件
+// 但是可以监测crtl+鼠标滚轮（缩放）事件和键盘事件(快捷键ctrl +/-/0缩放)来实现监听
+
+//c1 Ctrl+鼠标滚轮缩放
+document.addEventListener('DOMMouseScroll', function (e) {
+  e = e || window.event;
+  //监测滚轮事件中是否按下了Ctrl键
+  if (e.ctrlKey) {
+    setBaseFontSize()
   }
+})
 
-  let fontSize = viewportWidth * 100 / 1920 + 'px';
-  //eslint-disable-next-line
-  console.log('当前视口宽度' + viewportWidth + ',html基准字体大小' + fontSize);
-  return fontSize;
+//c2 键盘快捷键缩放
+document.addEventListener('keydown', function (e) {
+  e = e || window.event;
+  //按下Ctrl 以及以下任意一键：+ - 或 0
+  if (e.ctrlKey && (e.code === 'Equal' || e.code === 'Minus' || e.code === 'Digit0')) {
+    setBaseFontSize()
+  }
+})
+//===
+
+//设置html标签的基准字体
+function setBaseFontSize() {
+
+  let viewportWidth = document.documentElement.scrollWidth || document.body.scrollWidth || window.innerWidth || document.documentElement.getBoundingClientRect().width;
+
+
+  //浏览器缩放值(1表示未缩放)
+  const ratio = window.devicePixelRatio
+
+  //自定义默认缩放基础比例(为了将最终设置的值)
+  const defaultScale = 125
+
+  const fontSize = viewportWidth / 1920 * defaultScale * ratio + 'px';
+
+  document.querySelector('html').style.fontSize = fontSize;
 }
